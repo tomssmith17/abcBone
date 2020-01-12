cdc897181af84fb7c12d219efd5b3a0b7e73db5f
tomssmith17 <thsmith@clarku.edu>
2020-01-12 16:10:16 -0700

    reverted

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 hi = "hello world"
 
+<<<<<<< HEAD
 one = "number 1"
 
 two "number 2"
@@ -11,3 +12,6 @@ four = 345
 five = 789
 
 butWhy = "because I said so"
+=======
+one = "number 1"
+>>>>>>> parent of 3d3bf05... com2
