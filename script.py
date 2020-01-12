a964f93ab863005606d9e03fb7288590dbc8c22c
tomssmith17 <thsmith@clarku.edu>
2020-01-12 16:13:02 -0700

    staged revert

--- script.py
+++ script.py
@@ -1,17 +1,4 @@
 hi = "hello world"
 
-<<<<<<< HEAD
 one = "number 1"
 
-two "number 2"
-
-three = 3
-
-four = 345
-
-five = 789
-
-butWhy = "because I said so"
-=======
-one = "number 1"
->>>>>>> parent of 3d3bf05... com2
